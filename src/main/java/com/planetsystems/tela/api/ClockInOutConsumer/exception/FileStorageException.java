311f180bab8ac8517d10d3c5d5184576873ea73b
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-16 15:21:19 +0300

    added FileStorageException

---
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/exception/FileStorageException.java
@@ -0,0 +1,12 @@
+package com.planetsystems.tela.api.ClockInOutConsumer.exception;
+
+public class FileStorageException extends RuntimeException {
+	public FileStorageException(String message) {
+		super(message);
+	}
+
+	public FileStorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
