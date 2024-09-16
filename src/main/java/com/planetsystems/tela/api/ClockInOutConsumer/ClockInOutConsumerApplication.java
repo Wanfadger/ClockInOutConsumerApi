10296aae1b8548c308abd433c13d31a035d90a1b
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-16 16:24:41 +0300

    tested subject repository api

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/ClockInOutConsumerApplication.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/ClockInOutConsumerApplication.java
@@ -5,12 +5,16 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.planetsystems.tela.api.ClockInOutConsumer.Repository.AcademicTermRepository;
 import com.planetsystems.tela.api.ClockInOutConsumer.Repository.ClockInRepository;
 import com.planetsystems.tela.api.ClockInOutConsumer.Repository.SchoolRepository;
+import com.planetsystems.tela.api.ClockInOutConsumer.Repository.SubjectRepository;
 import com.planetsystems.tela.api.ClockInOutConsumer.Repository.projections.IdProjection;
 import com.planetsystems.tela.api.ClockInOutConsumer.dto.ClockInRequestDTO;
 import com.planetsystems.tela.api.ClockInOutConsumer.dto.MQResponseDto;
 import com.planetsystems.tela.api.ClockInOutConsumer.model.AcademicTerm;
 import com.planetsystems.tela.api.ClockInOutConsumer.model.School;
+import com.planetsystems.tela.api.ClockInOutConsumer.model.Subject;
+import com.planetsystems.tela.api.ClockInOutConsumer.model.enums.SchoolLevel;
 import com.planetsystems.tela.api.ClockInOutConsumer.model.enums.Status;
+import com.planetsystems.tela.api.ClockInOutConsumer.model.enums.SubjectClassification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -21,6 +25,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootApplication
@@ -32,6 +37,8 @@ public class ClockInOutConsumerApplication implements CommandLineRunner {
 	private final SchoolRepository schoolRepository;
 	private final AcademicTermRepository academicTermRepository;
 	private final ClockInRepository clockInRepository;
+	final SubjectRepository subjectRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ClockInOutConsumerApplication.class, args);
 
@@ -48,17 +55,19 @@ public class ClockInOutConsumerApplication implements CommandLineRunner {
 //		Optional<AcademicTerm> optionalAcademicTerm = academicTermRepository.activeAcademicTerm(Status.ACTIVE);
 //		System.out.println(optionalAcademicTerm.get().getTerm());
 //
-//		Optional<IdProjection> schoolIdByTelaNumberOptional = schoolRepository.findByTelaSchoolUIDAndStatusNot("8008226193412" , Status.DELETED);
-//		if (schoolIdByTelaNumberOptional.isPresent()) {
-//			IdProjection schoolIdProjection = schoolIdByTelaNumberOptional.get();
-//			Optional<School> schoolOptional = schoolRepository.findByStatusNotAndId(Status.DELETED, schoolIdProjection.getId());
-//
-//			log.info("school {} " , schoolOptional.get());
-//
-//
-//
+		Optional<IdProjection> schoolIdByTelaNumberOptional = schoolRepository.findByTelaSchoolUIDAndStatusNot("8008226193412" , Status.DELETED);
+		if (schoolIdByTelaNumberOptional.isPresent()) {
+			IdProjection schoolIdProjection = schoolIdByTelaNumberOptional.get();
+			School school = schoolRepository.findByStatusNotAndId(Status.DELETED, schoolIdProjection.getId()).orElseThrow(() -> new RuntimeException("school not found"));
+
+			log.info("school {} " , school.getName());
+			SchoolLevel schoolLevel = school.getSchoolLevel();
+			SubjectClassification subjectClassification = SubjectClassification.getSubjectClassification(schoolLevel.getLevel());
 //			clockInRepository.allClockByDate_SchoolWithStaff(LocalDate.now(), schoolIdProjection.getId());
-//		}
+			List<Subject> subjects = subjectRepository.findAllBySubjectClassificationNotNullAndStatusNotAndSubjectClassification(Status.DELETED, subjectClassification);
+			log.info("SUBJECTS {} " , subjects.size());
+			log.info("slclc {} " , subjectClassification.getSubjectClassification());
+		}
 
 
 
