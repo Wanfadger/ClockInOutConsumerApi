66979257d94f42d356e56a2b433fa05a09aee040
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-17 17:01:18 +0300

    added publishLearnerAttendance

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/service/consumer/SynchronizeMobileDataService.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/service/consumer/SynchronizeMobileDataService.java
@@ -2,6 +2,7 @@ package com.planetsystems.tela.api.ClockInOutConsumer.service.consumer;
 
 import com.planetsystems.tela.api.ClockInOutConsumer.model.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface SynchronizeMobileDataService {
@@ -9,9 +10,10 @@ public interface SynchronizeMobileDataService {
     void publishSchoolData(School school , AcademicTerm academicTerm);
     void publishSchoolClasses(School school, AcademicTerm academicTerm);
     void publishSchoolStaffs(School school, AcademicTerm academicTerm);
-    void publishSchoolClockIns(School school, AcademicTerm academicTerm);
+    void publishSchoolClockIns(School school, AcademicTerm academicTerm , String dateParam);
     void publishSubjects(School school, AcademicTerm academicTerm);
     void publishLearnerEnrollments(School school, AcademicTerm academicTerm);
+    void publishLearnerAttendance(School school, AcademicTerm academicTerm , String dateParam);
 
 
 
