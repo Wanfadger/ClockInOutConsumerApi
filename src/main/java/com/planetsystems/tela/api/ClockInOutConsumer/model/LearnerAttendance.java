72bd0f24a978ced20b80725edc0f35737bb3257f
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-17 16:57:33 +0300

    removed entity graphs

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/LearnerAttendance.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/LearnerAttendance.java
@@ -6,15 +6,20 @@ import java.time.*;
 
 
 //@Entity
-//@Table(name="LearnerAttendances",indexes = {@Index(columnList = "id,attendanceDate,girlsPresent,boysPresent,boysAbsent,comment")})
+
 //@Cache(region = "learnerAttendanceCache", usage = CacheConcurrencyStrategy.READ_WRITE)
 
-@NamedEntityGraph(name = "learnerAttendance-with-staffDetails-graph", attributeNodes = {
-		@NamedAttributeNode(value = "schoolStaff", subgraph = "staffDetails-sub-graph"),
-		@NamedAttributeNode(value = "academicTerm"), @NamedAttributeNode(value = "schoolClass"), }, subgraphs = {
-				@NamedSubgraph(name = "staffDetails-sub-graph", attributeNodes = {
-						@NamedAttributeNode("generalUserDetail"), }) })
+//@NamedEntityGraph(name = "learnerAttendance-with-staffDetails-graph", attributeNodes = {
+//		@NamedAttributeNode(value = "schoolStaff", subgraph = "staffDetails-sub-graph"),
+//		@NamedAttributeNode(value = "academicTerm"), @NamedAttributeNode(value = "schoolClass"), }, subgraphs = {
+//				@NamedSubgraph(name = "staffDetails-sub-graph", attributeNodes = {
+//						@NamedAttributeNode("generalUserDetail"), }) })
 @Entity(name = "LearnerAttendances")
+@Table(name="LearnerAttendances",indexes = {@Index(columnList = "attendanceDate,girlsPresent,boysPresent,boysAbsent,comment")})
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LearnerAttendance extends ParentEntity {
 
 	@OneToOne(fetch = FetchType.LAZY)
@@ -37,89 +42,4 @@ public class LearnerAttendance extends ParentEntity {
 	private long girlsAbsent;
 	private String comment;
 
-	public LearnerAttendance() {
-	}
-
-    @Transient()
-    private int localId;
-    
-   	public SchoolClass getSchoolClass() {
-		return schoolClass;
-	}
-
-	public void setSchoolClass(SchoolClass schoolClass) {
-		this.schoolClass = schoolClass;
-	}
-
-	public AcademicTerm getAcademicTerm() {
-		return academicTerm;
-	}
-
-	public void setAcademicTerm(AcademicTerm academicTerm) {
-		this.academicTerm = academicTerm;
-	}
-
-	public SchoolStaff getSchoolStaff() {
-		return schoolStaff;
-	}
-
-	public void setSchoolStaff(SchoolStaff schoolStaff) {
-		this.schoolStaff = schoolStaff;
-	}
-
-	public LocalDate getAttendanceDate() {
-		return attendanceDate;
-	}
-
-	public void setAttendanceDate(LocalDate attendanceDate) {
-		this.attendanceDate = attendanceDate;
-	}
-
-	public long getGirlsPresent() {
-		return girlsPresent;
-	}
-
-	public void setGirlsPresent(long girlsPresent) {
-		this.girlsPresent = girlsPresent;
-	}
-
-	public long getBoysPresent() {
-		return boysPresent;
-	}
-
-	public void setBoysPresent(long boysPresent) {
-		this.boysPresent = boysPresent;
-	}
-
-	public long getBoysAbsent() {
-		return boysAbsent;
-	}
-
-	public void setBoysAbsent(long boysAbsent) {
-		this.boysAbsent = boysAbsent;
-	}
-
-	public long getGirlsAbsent() {
-		return girlsAbsent;
-	}
-
-	public void setGirlsAbsent(long girlsAbsent) {
-		this.girlsAbsent = girlsAbsent;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-   
-    public int getLocalId() {
-        return localId;
-    }
-
-    public void setLocalId(int localId) {
-        this.localId = localId;
-    }
 }
