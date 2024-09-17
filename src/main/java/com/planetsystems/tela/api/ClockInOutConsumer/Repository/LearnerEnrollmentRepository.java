27b8a5b407554fef9f220e8f390128deb50ef8ce
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-17 17:01:05 +0300

    quick fix

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/Repository/LearnerEnrollmentRepository.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/Repository/LearnerEnrollmentRepository.java
@@ -2,8 +2,6 @@ package com.planetsystems.tela.api.ClockInOutConsumer.Repository;
 
 
 import com.planetsystems.tela.api.ClockInOutConsumer.model.LearnerEnrollment;
-import com.planetsystems.tela.api.ClockInOutConsumer.model.enums.Status;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
