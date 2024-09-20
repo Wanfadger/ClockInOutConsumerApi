06e49bf27fe03c12a3bd816d74ae770cc6db0a06
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-20 17:51:43 +0300

    added @SuperBuilder

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/LearnerEnrollment.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/LearnerEnrollment.java
@@ -4,6 +4,7 @@ package com.planetsystems.tela.api.ClockInOutConsumer.model;
 import com.planetsystems.tela.api.ClockInOutConsumer.model.enums.Status;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@SuperBuilder
 public class LearnerEnrollment extends ParentEntity{
 
 	@OneToOne(fetch = FetchType.LAZY)
