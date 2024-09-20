99ac1afb29080fabc83ba180b218cbe246ac7b46
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-20 17:52:44 +0300

    quick fix

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/SNLearnerEnrollment.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/SNLearnerEnrollment.java
@@ -3,6 +3,8 @@ package com.planetsystems.tela.api.ClockInOutConsumer.model;
 import com.planetsystems.tela.api.ClockInOutConsumer.model.enums.Status;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
+
 import java.io.Serializable;
 import java.time.*;
 import java.util.*;
@@ -10,6 +12,11 @@ import java.util.*;
 //Learners with special needs head count
 @Entity
 @Table(name = "SNLearnerEnrollments")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 public class SNLearnerEnrollment extends ParentEntity{
 
 	private static final long serialVersionUID = 1L;
@@ -27,66 +34,5 @@ public class SNLearnerEnrollment extends ParentEntity{
 	@OneToOne(fetch = FetchType.LAZY)
 	private SchoolStaff schoolStaff;
 
-	@Transient()
-	private int localId;
 
-	public SNLearnerEnrollment() {
-
-	}
-
-	public SchoolClass getSchoolClass() {
-		return schoolClass;
-	}
-
-	public void setSchoolClass(SchoolClass schoolClass) {
-		this.schoolClass = schoolClass;
-	}
-
-	public long getTotalBoys() {
-		return totalBoys;
-	}
-
-	public void setTotalBoys(long totalBoys) {
-		this.totalBoys = totalBoys;
-	}
-
-	public long getTotalGirls() {
-		return totalGirls;
-	}
-
-	public void setTotalGirls(long totalGirls) {
-		this.totalGirls = totalGirls;
-	}
-
-	public Status getEnrollmentStatus() {
-		return enrollmentStatus;
-	}
-
-	public void setEnrollmentStatus(Status enrollmentStatus) {
-		this.enrollmentStatus = enrollmentStatus;
-	}
-
-	public LocalDate getSubmissionDate() {
-		return submissionDate;
-	}
-
-	public void setSubmissionDate(LocalDate submissionDate) {
-		this.submissionDate = submissionDate;
-	}
-
-	public SchoolStaff getSchoolStaff() {
-		return schoolStaff;
-	}
-
-	public void setSchoolStaff(SchoolStaff schoolStaff) {
-		this.schoolStaff = schoolStaff;
-	}
-
-	public int getLocalId() {
-		return localId;
-	}
-
-	public void setLocalId(int localId) {
-		this.localId = localId;
-	}
 }
