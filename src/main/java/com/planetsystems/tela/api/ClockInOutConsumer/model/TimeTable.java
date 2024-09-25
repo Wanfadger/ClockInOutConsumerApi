9982db9d601c63a63064b74b5f8e51103b0c584b
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-25 17:47:54 +0300

    added TimetableDTO replaced getters and setters with lombok

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/TimeTable.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/TimeTable.java
@@ -5,20 +5,24 @@ import java.io.Serializable;
 import java.time.*;
 import java.util.*;
 
-@NamedEntityGraph(name = "timeTable-detail-graph" , attributeNodes = {
-		@NamedAttributeNode(value = "school"),
-		@NamedAttributeNode(value = "academicTerm"),
-		//@NamedAttributeNode(value = "timeTableLessons")
-}
-)
-@NamedEntityGraph(name = "timeTable-graph" , attributeNodes = {
-		@NamedAttributeNode(value = "school"),
-		//@NamedAttributeNode(value = "academicTerm"),
-		//@NamedAttributeNode(value = "timeTableLessons")
-}
-)
+//@NamedEntityGraph(name = "timeTable-detail-graph" , attributeNodes = {
+//		@NamedAttributeNode(value = "school"),
+//		@NamedAttributeNode(value = "academicTerm"),
+//		//@NamedAttributeNode(value = "timeTableLessons")
+//}
+//)
+//@NamedEntityGraph(name = "timeTable-graph" , attributeNodes = {
+//		@NamedAttributeNode(value = "school"),
+//		//@NamedAttributeNode(value = "academicTerm"),
+//		//@NamedAttributeNode(value = "timeTableLessons")
+//}
+//)
 @Entity
 @Table(name="TimeTables")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TimeTable extends ParentEntity {
 
 	@OneToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
@@ -33,36 +37,10 @@ public class TimeTable extends ParentEntity {
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, targetEntity = TimeTableLesson.class, mappedBy = "timeTable")
 	private List<TimeTableLesson> timeTableLessons;
 
-	public TimeTable() {
-	}
-
 	public TimeTable(String id) {
 		super(id);
 	}
 
-	public School getSchool() {
-		return school;
-	}
-
-	public void setSchool(School school) {
-		this.school = school;
-	}
-
-	public AcademicTerm getAcademicTerm() {
-		return academicTerm;
-	}
-
-	public void setAcademicTerm(AcademicTerm academicTerm) {
-		this.academicTerm = academicTerm;
-	}
-
-	public List<TimeTableLesson> getTimeTableLessons() {
-		return timeTableLessons;
-	}
-
-	public void setTimeTableLessons(List<TimeTableLesson> timeTableLessons) {
-		this.timeTableLessons = timeTableLessons;
-	}
 
 
 }
