62c52571ef71e8e7383753cf7d2fdb0a11addcd4
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-25 17:52:58 +0300

    used lombok

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/SNLearnerAttendance.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/SNLearnerAttendance.java
@@ -2,6 +2,8 @@ package com.planetsystems.tela.api.ClockInOutConsumer.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
+
 import java.io.Serializable;
 import java.time.*;
 import java.util.*;
@@ -15,6 +17,11 @@ import java.util.*;
 		@NamedAttributeNode(value = "schoolClass"),
 		@NamedAttributeNode(value = "academicTerm"),
 		@NamedAttributeNode(value = "schoolStaff") })
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 public class SNLearnerAttendance extends ParentEntity {
 
 
@@ -40,90 +47,4 @@ public class SNLearnerAttendance extends ParentEntity {
 	private long girlsAbsent;
 	private String comment;
 
-	@Transient()
-	private int localId;
-
-	public SNLearnerAttendance() {
-
-	}
-
-	public SchoolClass getSchoolClass() {
-		return schoolClass;
-	}
-
-	public void setSchoolClass(SchoolClass schoolClass) {
-		this.schoolClass = schoolClass;
-	}
-
-	public AcademicTerm getAcademicTerm() {
-		return academicTerm;
-	}
-
-	public void setAcademicTerm(AcademicTerm academicTerm) {
-		this.academicTerm = academicTerm;
-	}
-
-	public SchoolStaff getSchoolStaff() {
-		return schoolStaff;
-	}
-
-	public void setSchoolStaff(SchoolStaff schoolStaff) {
-		this.schoolStaff = schoolStaff;
-	}
-
-	public LocalDate getAttendanceDate() {
-		return attendanceDate;
-	}
-
-	public void setAttendanceDate(LocalDate attendanceDate) {
-		this.attendanceDate = attendanceDate;
-	}
-
-	public long getGirlsPresent() {
-		return girlsPresent;
-	}
-
-	public void setGirlsPresent(long girlsPresent) {
-		this.girlsPresent = girlsPresent;
-	}
-
-	public long getBoysPresent() {
-		return boysPresent;
-	}
-
-	public void setBoysPresent(long boysPresent) {
-		this.boysPresent = boysPresent;
-	}
-
-	public long getBoysAbsent() {
-		return boysAbsent;
-	}
-
-	public void setBoysAbsent(long boysAbsent) {
-		this.boysAbsent = boysAbsent;
-	}
-
-	public long getGirlsAbsent() {
-		return girlsAbsent;
-	}
-
-	public void setGirlsAbsent(long girlsAbsent) {
-		this.girlsAbsent = girlsAbsent;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-
-	public int getLocalId() {
-		return localId;
-	}
-
-	public void setLocalId(int localId) {
-		this.localId = localId;
-	}
 }
