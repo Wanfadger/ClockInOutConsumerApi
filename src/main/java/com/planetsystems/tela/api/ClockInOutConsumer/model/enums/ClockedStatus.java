fa9bb695a3ae73f47ce5f4cce039a1f849e2e4bd
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-26 09:12:37 +0300

    used lombok added fromString

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/enums/ClockedStatus.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/enums/ClockedStatus.java
@@ -1,29 +1,21 @@
 package com.planetsystems.tela.api.ClockInOutConsumer.model.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@Getter
+@AllArgsConstructor
 public enum ClockedStatus {
     CLOCKED_IN("Clocked In"),
     CLOCKED_OUT("Clocked Out");
 
-    private String Clocked;
+    private String clocked;
 
-    ClockedStatus(String clocked) {
-        Clocked = clocked;
-    }
 
-    public String getClocked() {
-        return Clocked;
-    }
-
-    public void setClocked(String clocked) {
-        Clocked = clocked;
-    }
-
-    public static ClockedStatus getClocked(String clocked){
-        for (ClockedStatus clockedStatus : ClockedStatus.values()) {
-            if (clockedStatus.getClocked().equalsIgnoreCase(clocked)) {
-                return clockedStatus;
-            }
-        }
-        return null;
+    public static Optional<ClockedStatus> fromString(String clockedStr){
+        return Arrays.stream(ClockedStatus.values()).parallel().filter(clockedStatus -> clockedStatus.clocked.equalsIgnoreCase(clockedStr)).findFirst();
     }
 }
