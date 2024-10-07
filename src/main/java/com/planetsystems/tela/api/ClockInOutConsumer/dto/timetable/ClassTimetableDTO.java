e3a4a153b51cb76714afd9c81de0ece0952eb74e
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-10-07 11:52:51 +0300

    default values to empty string

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/dto/timetable/ClassTimetableDTO.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/dto/timetable/ClassTimetableDTO.java
@@ -2,6 +2,7 @@ package com.planetsystems.tela.api.ClockInOutConsumer.dto.timetable;
 
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Setter
@@ -11,14 +12,14 @@ import java.util.List;
 @Builder
 @ToString
 public class ClassTimetableDTO {
-    private String id;
-    private String classId;
-    private String breakStartTime;
-    private String breakEndTime;
-    private String lunchStartTime;
-    private String lunchEndTime;
-    private String classStartTime;
-    private String classEndTime;
-    private int duration;
-    private List<TimeTableLessonDTO> lessons;
+    private String id = "";
+    private String classId = "";
+    private String breakStartTime = "";
+    private String breakEndTime = "";
+    private String lunchStartTime = "";
+    private String lunchEndTime = "";
+    private String classStartTime = "";
+    private String classEndTime = "";
+    private int duration = 0;
+    private List<TimeTableLessonDTO> lessons = new ArrayList<>();
 }
