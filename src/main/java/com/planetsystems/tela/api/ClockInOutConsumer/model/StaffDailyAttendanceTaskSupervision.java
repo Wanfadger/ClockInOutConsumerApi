645023f461891790b665996ab7fa7940d57b8670
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-10-24 11:14:48 +0300

    replaced getters and setters with lombok

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/StaffDailyAttendanceTaskSupervision.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/model/StaffDailyAttendanceTaskSupervision.java
@@ -3,6 +3,8 @@ package com.planetsystems.tela.api.ClockInOutConsumer.model;
 import com.planetsystems.tela.api.ClockInOutConsumer.model.enums.SupervisionStatus;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
+
 import java.io.Serializable;
 import java.time.*;
 import java.util.*;
@@ -12,6 +14,11 @@ import java.util.*;
  */
 @Entity
 @Table(name="StaffDailyAttendanceTaskSupervisions")
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StaffDailyAttendanceTaskSupervision extends ParentEntity{
 
     /**
@@ -20,65 +27,14 @@ public class StaffDailyAttendanceTaskSupervision extends ParentEntity{
     @ManyToOne(cascade = CascadeType.REFRESH , fetch = FetchType.LAZY)
     private StaffDailyTimeTableLesson staffDailyTimeTableLesson;
 
-
     @ManyToOne(cascade = CascadeType.REFRESH , fetch = FetchType.LAZY)
     @JoinColumn(name = "staffDailyAttendanceSupervision_id")
     private StaffDailyAttendanceSupervision staffDailyAttendanceSupervision;
 
-
-
     private SupervisionStatus teachingTimeStatus;
     
-    private SupervisionStatus teachingStatus;
+//    private SupervisionStatus teachingStatus;
     
     private String comment;
-
-    public StaffDailyAttendanceTaskSupervision(String id) {
-        super(id);
-    }
-
-    public StaffDailyAttendanceTaskSupervision() {
-    }
-
-    public StaffDailyAttendanceSupervision getStaffDailyAttendanceSupervision() {
-        return staffDailyAttendanceSupervision;
-    }
-
-    public void setStaffDailyAttendanceSupervision(StaffDailyAttendanceSupervision staffDailyAttendanceSupervision) {
-        this.staffDailyAttendanceSupervision = staffDailyAttendanceSupervision;
-    }
- 
-
-    public SupervisionStatus getTeachingTimeStatus() {
-        return teachingTimeStatus;
-    }
-
-    public void setTeachingTimeStatus(SupervisionStatus teachingTimeStatus) {
-        this.teachingTimeStatus = teachingTimeStatus;
-    }
-
-	public SupervisionStatus getTeachingStatus() {
-		return teachingStatus;
-	}
-
-	public void setTeachingStatus(SupervisionStatus teachingStatus) {
-		this.teachingStatus = teachingStatus;
-	}
-
-	public StaffDailyTimeTableLesson getStaffDailyTimeTableLesson() {
-		return staffDailyTimeTableLesson;
-	}
-
-	public void setStaffDailyTimeTableLesson(StaffDailyTimeTableLesson staffDailyTimeTableLesson) {
-		this.staffDailyTimeTableLesson = staffDailyTimeTableLesson;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
 
 }
