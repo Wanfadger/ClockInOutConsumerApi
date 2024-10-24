e4c5f6938bfecc29ac10c1f667640e786bad3b2b
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-10-24 11:08:54 +0300

    commented main test logs

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/ClockInOutConsumerApplication.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/ClockInOutConsumerApplication.java
@@ -3,12 +3,17 @@ package com.planetsystems.tela.api.ClockInOutConsumer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.planetsystems.tela.api.ClockInOutConsumer.Repository.*;
 import com.planetsystems.tela.api.ClockInOutConsumer.Repository.projections.IdProjection;
+import com.planetsystems.tela.api.ClockInOutConsumer.dto.supervision.StaffDailyAttendanceTaskSupervisionDTO;
 import com.planetsystems.tela.api.ClockInOutConsumer.dto.timetable.ClassTimetableDTO;
+import com.planetsystems.tela.api.ClockInOutConsumer.dto.timetable.StaffDailyTimetableDTO;
 import com.planetsystems.tela.api.ClockInOutConsumer.dto.timetable.TimeTableLessonDTO;
 import com.planetsystems.tela.api.ClockInOutConsumer.dto.timetable.TimetableDTO;
+import com.planetsystems.tela.api.ClockInOutConsumer.exception.TelaNotFoundException;
 import com.planetsystems.tela.api.ClockInOutConsumer.model.*;
 import com.planetsystems.tela.api.ClockInOutConsumer.model.enums.SchoolLevel;
 import com.planetsystems.tela.api.ClockInOutConsumer.model.enums.Status;
+import com.planetsystems.tela.api.ClockInOutConsumer.utils.Convertor;
+import com.planetsystems.tela.api.ClockInOutConsumer.utils.TelaDatePattern;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -18,8 +23,11 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableAsync
@@ -35,7 +43,12 @@ public class ClockInOutConsumerApplication implements CommandLineRunner {
 	final SNLearnerEnrollmentRepository snLearnerEnrollmentRepository;
 	final LearnerAttendanceRepository learnerAttendanceRepository;
 	final SNLearnerAttendanceRepository snLearnerAttendanceRepository;
-
+	final TimeTableLessonRepository timeTableLessonRepository;
+	final TimeTableRepository timeTableRepository;
+	final StaffDailyTimeTableRepository staffDailyTimeTableRepository;
+	final StaffDailyTimeTableLessonRepository staffDailyTimeTableLessonRepository;
+	final StaffDailyAttendanceSupervisionRepository staffDailyAttendanceSupervisionRepository;
+	final StaffDailyAttendanceTaskSupervisionRepository staffDailyAttendanceTaskSupervisionRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ClockInOutConsumerApplication.class, args);
 
@@ -49,32 +62,95 @@ public class ClockInOutConsumerApplication implements CommandLineRunner {
 //			System.out.println(optionalSchoolIdProjection.get().getId());
 //		}
 
-		Optional<AcademicTerm> optionalAcademicTerm = academicTermRepository.activeAcademicTerm(Status.ACTIVE);
+//		Optional<AcademicTerm> optionalAcademicTerm = academicTermRepository.activeAcademicTerm(Status.ACTIVE);
 //		System.out.println(optionalAcademicTerm.get().getTerm());
 ////8008226193412 , 8008229464166
-		Optional<IdProjection> schoolIdByTelaNumberOptional = schoolRepository.findByTelaSchoolUIDAndStatusNot("8008224447387" , Status.DELETED);
-		if (schoolIdByTelaNumberOptional.isPresent() && optionalAcademicTerm.isPresent()) {
-			AcademicTerm academicTerm = optionalAcademicTerm.get();
+//		Optional<IdProjection> schoolIdByTelaNumberOptional = schoolRepository.findByTelaSchoolUIDAndStatusNot("8008229464166" , Status.DELETED);
+//		if (schoolIdByTelaNumberOptional.isPresent() && optionalAcademicTerm.isPresent()) {
+//			AcademicTerm academicTerm = optionalAcademicTerm.get();
+//
+//
+//			IdProjection schoolIdProjection = schoolIdByTelaNumberOptional.get();
+//			School school = schoolRepository.findByStatusNotAndId(Status.DELETED, schoolIdProjection.getId()).orElseThrow(() -> new RuntimeException("school not found"));
+//
+//			log.info("school {} " , school.getName());
+//			log.info("school {} " , school.getId());
+//			log.info("academicTerm {} " , academicTerm.getId());
+//			SchoolLevel schoolLevel = school.getSchoolLevel();
+//
+//			ClassTimetableDTO classTimetableDTO = ClassTimetableDTO.builder()
+//					.lessons(List.of(TimeTableLessonDTO.builder().build()))
+//					.build();
+//
+//			TimetableDTO timetableDTO = TimetableDTO.builder()
+//					.id("")
+//					.schoolId("")
+//					.academicTermId("")
+//					.classTimetables(List.of(classTimetableDTO))
+//					.build();
 
 
-			IdProjection schoolIdProjection = schoolIdByTelaNumberOptional.get();
-			School school = schoolRepository.findByStatusNotAndId(Status.DELETED, schoolIdProjection.getId()).orElseThrow(() -> new RuntimeException("school not found"));
+//			List<StaffDailyTimeTable> termStaffDailyTimeTables = staffDailyTimeTableRepository.allByTerm_School("8a8089aa8fa5e464018fae49dac500ce", school.getId());
+//			log.info("termStaffDailyTimeTables {}  " , termStaffDailyTimeTables.size() );
+//			List<StaffDailyTimeTableLesson> termStaffDailyTimeTableLessons = staffDailyTimeTableLessonRepository.allIn(termStaffDailyTimeTables);
+//			log.info("termStaffDailyTimeTableLessons {}  " , termStaffDailyTimeTableLessons.size());
+//
+//			List<StaffDailyTimetableDTO> staffDailyTimetableDTOS = termStaffDailyTimeTables.parallelStream().flatMap(dailyTimeTable -> termStaffDailyTimeTableLessons.parallelStream()
+//					.filter(dailyLeLesson -> dailyTimeTable.getId().equals(dailyLeLesson.getStaffDailyTimeTable().getId()))
+//					.map(dailyLeLesson -> {
+//						StaffDailyTimetableDTO dto = StaffDailyTimetableDTO.builder()
+//								.lessonId(dailyLeLesson.getId())
+//								.actionStatus("")
+//								.submissionDate(dailyLeLesson.getLessonDate().format(TelaDatePattern.datePattern))
+//								.staffId(dailyTimeTable.getSchoolStaff().getId())
+//								.comment(dailyTimeTable.getComment())
+//								.build();
+//						return dto;
+//					})
+//			).sorted(Comparator.comparing(StaffDailyTimetableDTO::getStaffId)).toList();
+//			log.info("staffDailyTimetableDTOS {}  {} " , staffDailyTimetableDTOS , staffDailyTimetableDTOS.size() );
 
-			log.info("school {} " , school.getName());
-			log.info("school {} " , school.getId());
-			log.info("academicTerm {} " , academicTerm.getId());
-			SchoolLevel schoolLevel = school.getSchoolLevel();
 
-			ClassTimetableDTO classTimetableDTO = ClassTimetableDTO.builder()
-					.lessons(List.of(TimeTableLessonDTO.builder().build()))
-					.build();
+//			List<StaffDailyAttendanceSupervision> termStaffDailyAttendanceSupervisions = staffDailyAttendanceSupervisionRepository.allByTerm_School(academicTerm.getStartDate(), academicTerm.getEndDate(), school.getId());
+//			List<StaffDailyAttendanceTaskSupervision> termStaffDailyAttendanceTaskSupervisions = staffDailyAttendanceTaskSupervisionRepository.allIn(termStaffDailyAttendanceSupervisions);
+//
+//
+//			List<StaffDailyAttendanceTaskSupervisionDTO> staffDailyAttendanceTaskSupervisionDTOS = termStaffDailyAttendanceSupervisions.parallelStream().flatMap(attendanceSupervision -> termStaffDailyAttendanceTaskSupervisions.parallelStream()
+//							.filter(taskSupervision -> attendanceSupervision.getId().equals(taskSupervision.getStaffDailyAttendanceSupervision().getId()))
+//							.map(taskSupervision -> {
+//								StaffDailyAttendanceTaskSupervisionDTO dto = StaffDailyAttendanceTaskSupervisionDTO.builder()
+//										.id(taskSupervision.getId())
+//										.lessonTaskId(attendanceSupervision.getId())
+//										.supervisorId(attendanceSupervision.getSupervisor().getId())
+//										.supervisionDate(attendanceSupervision.getSupervisionDate().format(TelaDatePattern.datePattern))
+//										.supervisionTime(attendanceSupervision.getSupervisionTime().format(TelaDatePattern.timePattern24))
+//										.supervisionStatus(attendanceSupervision.getAttendanceStatus().getAttendance())
+//										.timeStatus( taskSupervision.getTeachingStatus() != null ?  taskSupervision.getTeachingStatus().getSupervision():"")
+//										.comment(taskSupervision.getComment())
+//										.build();
+//								return dto;
+//							})
+//					).sorted(Comparator.comparing(StaffDailyAttendanceTaskSupervisionDTO::getSupervisionDate))
+//					.toList();
+//			log.info("staffDailyAttendanceTaskSupervisionDTOS {}  {} " , staffDailyAttendanceTaskSupervisionDTOS , staffDailyAttendanceTaskSupervisionDTOS.size() );
 
-			TimetableDTO timetableDTO = TimetableDTO.builder()
-					.id("")
-					.schoolId("")
-					.academicTermId("")
-					.classTimetables(List.of(classTimetableDTO))
-					.build();
+//			IdProjection timetable = timeTableRepository.findBySchool_IdAndAcademicTerm_Id(school.getId() , "8a8089aa8fa5e464018fae49dac500ce").orElseThrow(() -> new TelaNotFoundException(school.getName() + "Timetable not found from " + academicTerm.getTerm()));
+//
+//			List<TimeTableLesson> timeTableLessons = timeTableLessonRepository.allByTimetable(timetable.getId()).parallelStream()
+//					.filter(Convertor.distinctByKey(timeTableLesson -> timeTableLesson.getStartTime() + "" + timeTableLesson.getEndTime() + "" + timeTableLesson.getLessonDay() + "" + timeTableLesson.getSubject().getId()))
+//
+//					.toList();
+//
+//			log.info("TIME LESSONS {} \n {} " , timeTableLessons.size() , timeTableLessons);
+//
+//			System.out.println();
+//			System.out.println();
+//
+//			Map<String, Long> stringLongMap = timeTableLessonRepository.allByTimetable(timetable.getId()).parallelStream()
+//					.filter(Convertor.distinctByKey(timeTableLesson -> timeTableLesson.getStartTime() + "" + timeTableLesson.getLessonDay() + "" + timeTableLesson.getSubject().getId()))
+//					.collect(Collectors.groupingBy(timeTableLesson -> timeTableLesson.getSchoolClass().getId(), Collectors.counting()));
+//			log.info("tim \n \n {}" , stringLongMap);
+
 
 //			log.info("TIME TABLE \n {} " , timetableDTO);
 
@@ -108,7 +184,7 @@ public class ClockInOutConsumerApplication implements CommandLineRunner {
 //		List<ClockIn> schoolDateClockIns = clockInRepository.allByTerm_SchoolWithStaff(academicTerm.getId(), school.getId());
 
 
-		}
+//		}
 
 
 
