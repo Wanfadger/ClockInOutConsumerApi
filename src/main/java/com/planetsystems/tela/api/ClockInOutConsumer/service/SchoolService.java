fb599eb8de5ebb392214e3fac278001e856b7e22
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-09 10:39:22 +0300

    quick fix

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/service/SchoolService.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/service/SchoolService.java
@@ -21,7 +21,7 @@ public class SchoolService {
     final SchoolRepository schoolRepository;
 
     public  ResponseEntity<SystemAppFeedBackDTO<SchoolDTO>> validateSchool(String telaSchoolNumber) {
-        IdProjection idProjection = schoolRepository.idByStatusAndTelaSchoolUID(Status.DELETED , telaSchoolNumber)
+        IdProjection idProjection = schoolRepository.idByStatusAndTelaSchoolUID(Status.DELETED , telaSchoolNumber )
                 .orElseThrow(() -> new TelaNotFoundException("Theres no school associated with " + telaSchoolNumber));
         School school = schoolRepository.findByStatusNotAndId(Status.DELETED, idProjection.getId()).orElseThrow(() -> new TelaNotFoundException("Theres no school associated with " + telaSchoolNumber));
         District district = school.getDistrict();
