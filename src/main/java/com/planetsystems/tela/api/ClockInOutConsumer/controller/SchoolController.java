91a3f1f4c5b0188098a01221a32cff3513bd3a35
fkasoma <147488985+fkasoma@users.noreply.github.com>
2024-11-11 16:30:37 +0300

    Update SchoolController.java-SynchronizeRestSchoolDataDTO

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/controller/SchoolController.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/controller/SchoolController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.planetsystems.tela.api.ClockInOutConsumer.dto.SynchronizeRestSchoolDataDTO;
 
 @RestController
 @RequiredArgsConstructor
