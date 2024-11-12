e8fc3ebf2a570629ca6ae2dcc4c9d75b8886eefa
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-12 17:07:49 +0300

    added CacheService with following services
        AcademicTermDTO cacheActiveAcademicTerm();
        SchoolDTO cacheSchoolData(String telaSchoolNumber);
        MQResponseDto<List<ClassDTO>> cacheSchoolClasses(SchoolDTO schoolDTO);
        MQResponseDto<List<StaffDTO>> cacheSchoolStaffs(SchoolDTO schoolDTO);
        MQResponseDto<List<ClockInDTO>> cacheSchoolTermClockIns(SchoolDTO schoolDTO);
        MQResponseDto<List<ClockOutDTO>> cacheSchoolTermClockOuts(SchoolDTO schoolDTO);
        MQResponseDto<List<IdNameCodeDTO>> cacheSubjects(SchoolDTO schoolDT);
        MQResponseDto<List<LearnerHeadCountDTO>> cacheLearnerEnrollments(SchoolDTO schoolDTO);
        MQResponseDto<List<LearnerAttendanceDTO>> cacheLearnerAttendance(SchoolDTO schoolDTO);
        MQResponseDto<List<StaffDailyTimeAttendanceDTO>> cacheStaffDailyTimeAttendanceSupervision(SchoolDTO schoolDTO, String dateParam);
        MQResponseDto<List<StaffDailyAttendanceTaskSupervisionDTO>> cacheStaffDailyTimetableTaskSupervision(SchoolDTO schoolDTO , String dateParam);
    
        MQResponseDto<List<StaffDailyTimetableDTO>> cacheStaffDailyTimetables(SchoolDTO schoolDTO);
        MQResponseDto<List<DistrictDTO>> cacheDistricts();
        MQResponseDto<TimetableDTO> cacheSchoolTimetables(SchoolDTO schoolDTO);

---
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/service/cache/CacheService.java
@@ -0,0 +1,29 @@
+package com.planetsystems.tela.api.ClockInOutConsumer.service.cache;
+
+
+
+import com.planetsystems.tela.api.ClockInOutConsumer.dto.*;
+import com.planetsystems.tela.api.ClockInOutConsumer.dto.supervision.StaffDailyAttendanceTaskSupervisionDTO;
+import com.planetsystems.tela.api.ClockInOutConsumer.dto.timetable.StaffDailyTimetableDTO;
+import com.planetsystems.tela.api.ClockInOutConsumer.dto.timetable.TimetableDTO;
+
+import java.util.List;
+
+public interface CacheService {
+
+    AcademicTermDTO cacheActiveAcademicTerm();
+    SchoolDTO cacheSchoolData(String telaSchoolNumber);
+    MQResponseDto<List<ClassDTO>> cacheSchoolClasses(SchoolDTO schoolDTO);
+    MQResponseDto<List<StaffDTO>> cacheSchoolStaffs(SchoolDTO schoolDTO);
+    MQResponseDto<List<ClockInDTO>> cacheSchoolTermClockIns(SchoolDTO schoolDTO);
+    MQResponseDto<List<ClockOutDTO>> cacheSchoolTermClockOuts(SchoolDTO schoolDTO);
+    MQResponseDto<List<IdNameCodeDTO>> cacheSubjects(SchoolDTO schoolDT);
+    MQResponseDto<List<LearnerHeadCountDTO>> cacheLearnerEnrollments(SchoolDTO schoolDTO);
+    MQResponseDto<List<LearnerAttendanceDTO>> cacheLearnerAttendance(SchoolDTO schoolDTO);
+    MQResponseDto<List<StaffDailyTimeAttendanceDTO>> cacheStaffDailyTimeAttendanceSupervision(SchoolDTO schoolDTO, String dateParam);
+    MQResponseDto<List<StaffDailyAttendanceTaskSupervisionDTO>> cacheStaffDailyTimetableTaskSupervision(SchoolDTO schoolDTO , String dateParam);
+
+    MQResponseDto<List<StaffDailyTimetableDTO>> cacheStaffDailyTimetables(SchoolDTO schoolDTO);
+    MQResponseDto<List<DistrictDTO>> cacheDistricts();
+    MQResponseDto<TimetableDTO> cacheSchoolTimetables(SchoolDTO schoolDTO);
+}
