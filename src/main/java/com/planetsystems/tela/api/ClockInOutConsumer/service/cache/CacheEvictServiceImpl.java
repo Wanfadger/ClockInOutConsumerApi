c8b65245220fa289ca10a51e62e37cb39efd0601
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-26 16:30:07 +0300

    update cacheEvict configs - removed allEntries key

--- src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/service/cache/CacheEvictServiceImpl.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutConsumer/service/cache/CacheEvictServiceImpl.java
@@ -9,39 +9,38 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class CacheEvictServiceImpl implements CacheEvictService {
-    final String CACHE_PACKAGE_STRUCTURE = "com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.beanConfig.";
     @Override
-    @CacheEvict(value = CACHE_PACKAGE_STRUCTURE+CacheKeys.ACTIVE_ACADEMIC_TERM , cacheManager = "monthCacheManager" , allEntries = true)
+    @CacheEvict(value = CacheKeys.ACTIVE_ACADEMIC_TERM , cacheManager = "monthCacheManager" )
     public void evictActiveAcademicTerm() {
      log.info("evictActiveAcademicTerm");
     }
 
     @Override
-    @CacheEvict(value = CACHE_PACKAGE_STRUCTURE+CacheKeys.SCHOOL , key = "#telaSchoolNumber" , allEntries = true)
+    @CacheEvict(value = CacheKeys.SCHOOL , key = "#telaSchoolNumber")
     public void evictSchoolData(String telaSchoolNumber) {
-        log.info("evictSchoolData {} {} " , telaSchoolNumber , CACHE_PACKAGE_STRUCTURE+CacheKeys.SCHOOL);
+        log.info("evictSchoolData {} {} " , telaSchoolNumber , CacheKeys.SCHOOL);
     }
 
     @Override
-    @CacheEvict(value = CacheKeys.CLASSES , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}" , allEntries = true)
+    @CacheEvict(value = CacheKeys.CLASSES , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}")
     public void evictSchoolClasses(String telaSchoolNumber, String academicTerm) {
         log.info("evictSchoolClasses {}  {} " , telaSchoolNumber , academicTerm);
     }
 
     @Override
-    @CacheEvict(value = CacheKeys.STAFFS , key = "#telaSchoolNumber" , allEntries = true)
+    @CacheEvict(value = CacheKeys.STAFFS , key = "#telaSchoolNumber" )
     public void evictSchoolStaffs(String telaSchoolNumber, String academicTerm) {
         log.info("evictSchoolStaffs {}  {} " , telaSchoolNumber , academicTerm);
     }
 
     @Override
-    @CacheEvict(value = CacheKeys.CLOCKINS , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}" , allEntries = true)
+    @CacheEvict(value = CacheKeys.CLOCKINS , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}" )
     public void evictSchoolTermClockIns(String telaSchoolNumber, String academicTerm) {
         log.info("evictSchoolTermClockIns {}  {} " , telaSchoolNumber , academicTerm);
     }
 
     @Override
-    @CacheEvict(value = CacheKeys.CLOCKOUTS , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}" , allEntries = true)
+    @CacheEvict(value = CacheKeys.CLOCKOUTS , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}" )
     public void evictSchoolTermClockOuts(String telaSchoolNumber, String academicTerm) {
         log.info("evictSchoolTermClockOuts {}  {} " , telaSchoolNumber , academicTerm);
     }
@@ -53,31 +52,31 @@ public class CacheEvictServiceImpl implements CacheEvictService {
     }
 
     @Override
-    @CacheEvict(value = CacheKeys.LEARNER_HEADCOUNTS , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}" , allEntries = true)
+    @CacheEvict(value = CacheKeys.LEARNER_HEADCOUNTS , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}" )
     public void evictLearnerEnrollments(String telaSchoolNumber, String academicTerm) {
      log.info("evictLearnerEnrollments {}  {} " , telaSchoolNumber , academicTerm);
     }
 
     @Override
-    @CacheEvict(value = CacheKeys.LEARNER_ATTENDANCES , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}" , allEntries = true)
+    @CacheEvict(value = CacheKeys.LEARNER_ATTENDANCES , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}")
     public void evictLearnerAttendance(String telaSchoolNumber, String academicTerm) {
      log.info("evictLearnerAttendance {}  {} " , telaSchoolNumber , academicTerm);
     }
 
     @Override
-    @CacheEvict(value = CacheKeys.STAFF_DAILY_TIME_ATTENDANCES , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}" , allEntries = true)
+    @CacheEvict(value = CacheKeys.STAFF_DAILY_TIME_ATTENDANCES , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}" )
     public void evictStaffDailyTimeAttendanceSupervision(String telaSchoolNumber, String academicTerm) {
      log.info("evictStaffDailyTimeAttendanceSupervision {}  {} " , telaSchoolNumber , academicTerm);
     }
 
     @Override
-    @CacheEvict(value = CacheKeys.STAFF_DAILY_TASK_SUPERVISIONS , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}", allEntries = true)
+    @CacheEvict(value = CacheKeys.STAFF_DAILY_TASK_SUPERVISIONS , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}")
     public void evictStaffDailyTimetableTaskSupervision(String telaSchoolNumber, String academicTerm) {
      log.info("evictStaffDailyTimetableTaskSupervision {}  {} " , telaSchoolNumber , academicTerm);
     }
 
     @Override
-    @CacheEvict(value = CacheKeys.STAFF_DAILY_TIMETABLES , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}", allEntries = true)
+    @CacheEvict(value = CacheKeys.STAFF_DAILY_TIMETABLES , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}")
     public void evictStaffDailyTimetables(String telaSchoolNumber, String academicTerm) {
      log.info("evictStaffDailyTimetables {}  {} " , telaSchoolNumber , academicTerm);
     }
@@ -89,7 +88,7 @@ public class CacheEvictServiceImpl implements CacheEvictService {
     }
 
     @Override
-    @CacheEvict(value = CacheKeys.SCHOOL_TIMETABLE , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}", allEntries = true)
+    @CacheEvict(value = CacheKeys.SCHOOL_TIMETABLE , key = "{'school='+#telaSchoolNumber+',term='+#academicTerm}")
     public void evictSchoolTimetables(String telaSchoolNumber, String academicTerm) {
         log.info("evictSchoolTimetables {}  {} " , telaSchoolNumber , academicTerm);
     }
